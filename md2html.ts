4c33f1beb94615962aadb161cffb07ae8f707129
M.D. van Es <es@mdworld.nl>
2021-04-06 18:24:31 +0200

    Get heading

--- md2html.ts
+++ md2html.ts
@@ -1,5 +1,7 @@
 // import { Marked } from "https://deno.land/x/markdown@v2.0.0/mod.ts";
-import { Marked } from "markdown";
+// import { Marked, Renderer } from "markdown";
+// import { Node } from "https://deno.land/x/markdown_tree@1.1.0/mod.ts";
+import { Node } from "markdown_tree";
 
 const decoder = new TextDecoder("utf-8");
 const filename = Deno.args[0];
@@ -7,8 +9,25 @@ const filename = Deno.args[0];
 const markdown = decoder.decode(await Deno.readFile(filename));
 // console.log(markdown);
 // Marked.setOptions({
-
+//     renderer: new Renderer()
 // });
-const markup = Marked.parse(markdown);
-console.log(markup.content);
-console.log(JSON.stringify(markup.meta));
+// const markup = Marked.parse(markdown);
+// console.log(markup.content);
+// console.log(JSON.stringify(markup.meta));
+const x = Node.from(markdown);
+
+// TODO use recursion and type guards
+function digForHeadingContent(n: Node) {
+    if(!n.children) {
+        throw Error("no children");
+    }
+    const heading = n.children.filter(h => h.props).find(h => h.props && h.props.type === "heading");
+    if(!heading) {
+        throw Error("no heading");
+    }
+    const t = heading.children?.find(h => h.props && h.props.type === "text");
+    // @ts-ignore
+    return t.props && t.props.content;
+}
+
+console.log(digForHeadingContent(x));
