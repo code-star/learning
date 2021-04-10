49f65ce9f036099a34fe1b94fdc9c5783d6ac067
M.D. van Es <es@mdworld.nl>
2021-04-10 09:24:16 +0200

    Add build script

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import generateHtml from "./generateHtml.ts";
 import convertFileDataListToTree from "./convertFileDataListToTree.ts";
 import { parseFilesOnPaths } from "./parseFiles.ts";
+import { ensureDirSync } from "fs";
 
 const parsedFiles = await parseFilesOnPaths(Deno.args.slice(0,2));
 const resultsTree = convertFileDataListToTree(parsedFiles);
 const outputHtml = generateHtml(resultsTree);
 
-Deno.writeTextFileSync("./index.html", outputHtml);
-console.log("Wrote to index.html");
+ensureDirSync("./dist");
+Deno.writeTextFileSync("./dist/index.html", outputHtml);
+console.log("Wrote to ./dist/index.html");
