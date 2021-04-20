bdb75c31516d6e30d32bbec045a8057b101dcda0
M.D. van Es <es@mdworld.nl>
2021-04-20 14:28:05 +0200

    Fix generated import

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-// import generateHtml from "./generateHtml.ts";
 import convertFileDataListToTree from "./convertFileDataListToTree.ts";
 import { parseFilesOnPaths } from "./parseFiles.ts";
 import { ensureDirSync } from "fs";
 
+const OUTPUT_PATH = "./src/client/generated/_guide.tsx";
+
 const parsedFiles = await parseFilesOnPaths(Deno.args.slice(0,2));
 const resultsTree = convertFileDataListToTree(parsedFiles);
-// const outputHtml = generateHtml(resultsTree);
 
-ensureDirSync("./src/generated");
-Deno.writeTextFileSync("./src/generated/_guide.ts", `export const guide = ${JSON.stringify(resultsTree)};`);
-console.log("Wrote to ./src/generated/_guide.ts");
+ensureDirSync("./src/client/generated");
+Deno.writeTextFileSync(OUTPUT_PATH, `export const guide = ${JSON.stringify(resultsTree)};`);
+console.log(`Wrote to ${OUTPUT_PATH}`);
