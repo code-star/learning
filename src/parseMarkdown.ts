edac67056d2ffa56acd0fd5cb37145dc1fa9135a
M.D. van Es <es@mdworld.nl>
2021-04-21 15:59:10 +0200

    Clean up

--- src/parseMarkdown.ts
+++ src/parseMarkdown.ts
@@ -1,15 +1,6 @@
 import { Node } from "markdown_tree";
+// import { Node } from "./treetest.ts";
 import { CustomNode } from "./types.ts";
-
-// const getContent = (n: Node): string => {
-//   if (n?.props?.type === "paragraph") {
-//     const texts = n?.children?.filter((c) => c.props?.type === "text") ?? [];
-//     const text = texts.map((t) => (t?.props as any).content).join(" ");
-//     return text;
-//   }
-//   console.log(n.xml("html", Node.HTMLHelper()))
-//   return "*not p element, not yet supported*";
-// };
 
 const isCustomNode = (n: Node | CustomNode): n is CustomNode => {
   return (n as CustomNode).type && (n as CustomNode).type === "topic-content";
@@ -25,10 +16,6 @@ const reduceNodesToGroup = (
   }
   const last = acc[acc.length - 1];
   if (last) {
-    // if (!last.paragraph) {
-    //   last.paragraph = "";
-    // }
-    // last.paragraph.push(getContent(next));
     last.section += next.xml("html", Node.HTMLHelper());
   } else {
     console.error("no group exists", acc, next);
@@ -52,9 +39,7 @@ export function digForHeadingContent(n: Node): CustomNode[] {
     .map((child): CustomNode | Node => {
       const heading =
         child?.props?.type === "heading" && getHeadingContent(child);
-      return heading
-        ? { type: "topic-content", heading, section: "" }
-        : child;
+      return heading ? { type: "topic-content", heading, section: "" } : child;
     })
     .reduce(reduceNodesToGroup, []);
   return newNode;
