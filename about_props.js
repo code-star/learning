5eb23c246ebed63c9016415f99bf38c08d27ce97
mdvanes <es@mdworld.nl>
2021-04-21 06:28:38 +0000

    Deploying to gh-pages from @ code-star/learning@93c7e07f2b20c6a5045dd7ba2d966c1e0462afc8 🚀

--- about_props.js
+++ about_props.js
@@ -4,27 +4,27 @@ export default {
     'pagePath': "about.md",
     'layoutPath': "_layout.tsx",
     'outputPath': "about.html",
-    'title': "Codestar Learning",
+    'title': "About Codestar Learning",
     'content': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: '<h1>Codestar Learning</h1>'
+            __html: '<h1>About Codestar Learning</h1>\n<p>Read here about our study guide.</p>'
         } }),
     'head': null,
     'script': React.createElement(React.Fragment, null,
         React.createElement("script", { src: "https://cdn.pagic.org/react@16.13.1/umd/react.production.min.js" }),
         React.createElement("script", { src: "https://cdn.pagic.org/react-dom@16.13.1/umd/react-dom.production.min.js" }),
         React.createElement("script", { src: "/learning/index.js", type: "module" })),
-    'contentTitle': React.createElement("h1", { key: "0" }, "Codestar Learning"),
+    'contentTitle': React.createElement("h1", { key: "0" }, "About Codestar Learning"),
     'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: ''
+            __html: '<p>Read here about our study guide.</p>'
         } }),
     'toc': null,
-    'author': "mdvanes",
+    'author': "M.D. van Es",
     'contributors': [
-        "mdvanes"
+        "M.D. van Es"
     ],
-    'date': "2021-04-20T12:36:41.000Z",
+    'date': "2021-04-21T06:26:09.000Z",
     'updated': null,
-    'excerpt': "",
+    'excerpt': "Read here about our study guide.",
     'cover': undefined,
     'blog': {
         "isPost": false,
