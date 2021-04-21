c8dab9e1fcbe52051d49bf87569eba1f62a6376e
mdvanes <es@mdworld.nl>
2021-04-21 09:34:20 +0000

    Deploying to gh-pages from @ code-star/learning@2acb66909bc4896929bb918655e4cbc6980aa0e2 🚀

--- components/_FoldableListItem.js
+++ components/_FoldableListItem.js
@@ -1,14 +1,21 @@
 import RenderTree from "./_RenderTree.js";
-const FoldableListItem = ({ label, tree }) => {
+export const contentToListItems = (pathSegments) => (c) => (React.createElement("li", { style: { cursor: "help" }, title: c.paragraph.join("\n") },
+    "\uD83D\uDCDA ",
+    c.heading,
+    " ",
+    React.createElement("a", { href: pathSegments.join("/") }, "details")));
+const FoldableListItem = ({ label, content, tree }) => {
     const [isFolded, setIsFolded] = React.useState(true);
-    const clickableLabel = (React.createElement("span", { onClick: () => {
+    const clickableLabel = (React.createElement("span", { style: { cursor: "pointer" }, onClick: () => {
             setIsFolded(!isFolded);
         } },
         label,
         " ",
         isFolded ? "ᐅ" : "▼"));
+    const contentStr = content ? content.map(contentToListItems([])) : "";
     const result = isFolded ? ("") : (React.createElement("ul", null,
-        React.createElement(RenderTree, { topicTree: tree })));
+        React.createElement(RenderTree, { topicTree: tree }),
+        contentStr));
     return (React.createElement("li", null,
         clickableLabel,
         result));
--- components/_RenderTree.js
+++ components/_RenderTree.js
@@ -1,5 +1,5 @@
 import { formatFileName } from "../utils/_formatters.js";
-import FoldableListItem from "./_FoldableListItem.js";
+import FoldableListItem, { contentToListItems } from "./_FoldableListItem.js";
 const byKey = (entry, otherEntry) => {
     if (entry[0] > otherEntry[0]) {
         return 1;
@@ -9,15 +9,20 @@ const byKey = (entry, otherEntry) => {
     }
     return 0;
 };
+const distillFileData = (node) => {
+    const { pathSegments, content, name, ...rest } = node;
+    const fileData = { content, name, pathSegments };
+    return [fileData, rest];
+};
 const RenderTree = ({ topicTree }) => {
     const listStr = Object.entries(topicTree)
         .sort(byKey)
         .map(([entryKey, entryVal]) => {
-        const { pathSegments, content, name, ...rest } = entryVal;
-        const contentStr = content ? (React.createElement("ul", null,
-            React.createElement("li", null, content))) : ("");
+        const [fileData, rest] = distillFileData(entryVal);
+        const { pathSegments, content, name } = fileData;
+        const contentStr = content ? (React.createElement("ul", null, content.map(contentToListItems(pathSegments)))) : ("");
         const hasRest = Object.keys(rest).length > 0;
-        return hasRest ? (React.createElement(FoldableListItem, { label: entryKey, tree: rest })) : (React.createElement("li", null,
+        return hasRest ? (React.createElement(FoldableListItem, { label: entryKey, content: content, tree: rest })) : (React.createElement("li", null,
             formatFileName(entryKey),
             " ",
             contentStr));
