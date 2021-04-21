98d129d033bcb824fee0c0fa3025f848ecdc405a
M.D. van Es <es@mdworld.nl>
2021-04-21 11:13:19 +0200

    Try collect paragraphs

--- src/parseMarkdown.ts
+++ src/parseMarkdown.ts
@@ -1,12 +1,65 @@
 import { Node } from "markdown_tree";
 
-// TODO use recursion and type guards
+interface CustomNode {
+  type: "topic-content";
+  heading: string;
+  paragraph: string[];
+}
+
+const getContent = (n: Node): string => {
+  if (n?.props?.type === "paragraph") {
+    const texts = n?.children?.filter((c) => c.props?.type === "text") ?? [];
+    const text = texts.map((t) => (t?.props as any).content).join(" ");
+    return text;
+  }
+  return "*not p element, not yet supported*";
+};
+
+const isCustomNode = (n: Node | CustomNode): n is CustomNode => {
+  return (n as CustomNode).type && (n as CustomNode).type === "topic-content";
+}
+
+const reduceNodesToGroup = (acc: CustomNode[], next: Node | CustomNode): CustomNode[] => {
+  if (isCustomNode(next)) {
+    acc.push(next);
+    return acc;
+  }
+  const last = acc[acc.length - 1];
+  if (last) {
+    if (!last.paragraph) {
+      last.paragraph = [];
+    }
+    last.paragraph.push(getContent(next));
+  } else {
+    console.error("no group exists");
+  }
+  return acc;
+};
+
 export function digForHeadingContent(n: Node): string[] {
   if (!n.children) {
-    // throw Error("no children");
-    // console.log(n, "no children");
     return [];
   }
+
+  const newNode = n.children
+    .map((child): CustomNode | Node => {
+      const heading =
+        child?.props?.type === "heading" &&
+        child.children
+          ?.filter((h) => h && h.props && h.props.type === "text")
+          .map((t) => t && t.props && (t.props as any).content)
+          .join();
+      // const heading = y ? y : false;
+      // if (heading) {
+      //   return heading;
+      // }
+      // return child;
+      return heading ? { type: "topic-content", heading, paragraph: [] } : child;
+    })
+    .reduce(reduceNodesToGroup, []);
+  console.log(newNode);
+
+  // TODO Fix this garbage
   const headings = n.children
     .filter((h) => h.props)
     .filter((h) => h.props && h.props.type === "heading")
@@ -14,6 +67,7 @@ export function digForHeadingContent(n: Node): string[] {
     .filter((h) => h && h.props && h.props.type === "text")
     // @ts-expect-error
     .map((t) => t && t.props && t.props.content);
+  // console.log(n, headings);
   return headings;
   // if (!heading) {
   //   // throw Error("no heading");
